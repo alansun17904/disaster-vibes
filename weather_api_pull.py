cc4cbf4920130ba97939a5b46288be2aa404a712
habbuxx <harryxiao66@hotmail.com>
2020-09-13 10:10:27 +0800

    Changed lat and long to see rain_mm diff.

--- weather_api_pull.py
+++ weather_api_pull.py
@@ -7,14 +7,14 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-# latitude and longitude of New York
-lat = str(40.7)
-long = str(-73.9)
+# latitude and longitude of Pennsylvania
+lat = str(41.2)
+long = str(-77)
 
 response = requests.get('https://api.meteomatics.com/2020-09-12T00:00:00ZP1D:PT24H/precip_24h:mm/40.7,-73.9/json',
                         auth=HTTPBasicAuth('columbia_xiao', '45Vcg7owWqZLK'))
 print(response.status_code)
-#jprint(response.json())
+jprint(response.json())
 
 """ Saves the rainfall over 24hrs data into a list called rain_mm """
 raw_rain_mm = response.json()['data'][0]['coordinates'][0]['dates']
